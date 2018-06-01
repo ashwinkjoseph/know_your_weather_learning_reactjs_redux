588dd61cea83231cfd0bc305039ab5be6134d8a3
Joseph Ashwin <ashwinkjoseph@gmail.com>
2018-06-01 09:04:46 +0530

    After Adding the redux-promise middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-// import ReduxPromise from 'redux-promise'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise'
 
 import SearchResults from "./pages/searchResults/index";
 import reducers from './reducers/index'
 
-// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 const App = ()=>{
     return (
@@ -18,7 +18,9 @@ const App = ()=>{
 }
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={createStoreWithMiddleware(reducers)}>
+    <App/>
+    </Provider>,
     document.getElementById('app')
 );
 
